f430a290947b3f7e6831a217f3229149cb1943f3
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-06-06 10:55:01 +0200

    add new instance scanner and user inputs to PasswordGenerator

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,10 +1,25 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		// new instance scanner
+		Scanner scanner = new Scanner(System.in);
+		// user inputs
+		System.out.println("inserisci il tuo nome:");
+		String userFirstName = scanner.nextLine();
+		System.out.println("inserisci il tuo cognome:");
+		String userLastName = scanner.nextLine();
+		System.out.println("inserisci il tuo colore preferito:");
+		String userFavColour = scanner.nextLine();
+		System.out.println("inserisci il giorno in cui sei nato:");
+		byte userBirthDay = (byte) Integer.parseInt(scanner.nextLine());
+		System.out.println("inserisci il mese in cui sei nato:");
+		byte userBirthMonth = (byte) Integer.parseInt(scanner.nextLine());
+		System.out.println("inserisci il tuo anno di nascita:");
+		short userBirthYear = (short) Integer.parseInt(scanner.nextLine());
+		}
 
 }
